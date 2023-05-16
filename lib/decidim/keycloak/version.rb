25b986162134084a0e49bb490155d88dc30e37be
Francisco Bolívar <francisco.bolivar@nazaries.com>
2023-05-16 15:06:59 +0200

    Update version

--- lib/decidim/keycloak/version.rb
+++ lib/decidim/keycloak/version.rb
@@ -3,7 +3,7 @@
 module Decidim
   # This holds the decidim-meetings version.
   module Keycloak
-    VERSION = "1.0.0"
+    VERSION = "0.1.0"
     DECIDIM_VERSION = "0.26.5"
     COMPAT_DECIDIM_VERSION = [">= 0.25.0", "< 0.28"].freeze
   end
