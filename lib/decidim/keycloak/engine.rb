981751bb61cde1b8e09bc9c979caca6b862dabf1
xdubx <klaus.boldt97@gmail.com>
2025-01-04 19:42:58 +0100

    move redirect url

--- lib/decidim/keycloak/engine.rb
+++ lib/decidim/keycloak/engine.rb
@@ -19,11 +19,11 @@ module Decidim
             config = organization.enabled_omniauth_providers[:keycloakopenid]
             # remove the language parameter from the callback url
             # env["omniauth.strategy"].options[:client_options][:redirect_uri] = request.url.split("?").first
-            env["omniauth.strategy"].options[:name] = "Login with Keycloak" # replace with config name
-            env["omniauth.strategy"].options[:callback_url] = request.url.split("?").first
+            #env["omniauth.strategy"].options[:name] = "Login with Keycloak" # replace with config name
+           # env["omniauth.strategy"].options[:callback_path] = request.url.split("?").first
             env["omniauth.strategy"].options[:client_id] = config[:client_id]
             env["omniauth.strategy"].options[:client_secret] = config[:client_secret]
-            env["omniauth.strategy"].options[:client_options] = { site: config[:site], realm: config[:realm], base_url: config[:base_url], button_text: config[:button_text] }
+            env["omniauth.strategy"].options[:client_options] = { site: config[:site], realm: config[:realm], base_url: config[:base_url], redirect_uri: "/users/auth/keycloakopenid/callback"}
           }
         end
       end
