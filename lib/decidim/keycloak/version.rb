a922083c80cf0c761b9ffe3c80c78bf929f2fc31
xdubx <klaus.boldt97@gmail.com>
2025-04-03 00:08:59 +0200

    freeze to 29.1

--- lib/decidim/keycloak/version.rb
+++ lib/decidim/keycloak/version.rb
@@ -5,6 +5,6 @@ module Decidim
   module Keycloak
     VERSION = "0.1.1"
     DECIDIM_VERSION = "0.29.0"
-    COMPAT_DECIDIM_VERSION = [">= 0.27.0", "< 0.30"].freeze
+    COMPAT_DECIDIM_VERSION = [">= 0.27.0", "< 0.29.1"].freeze
   end
 end
